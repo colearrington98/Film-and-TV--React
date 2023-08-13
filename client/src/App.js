55f976d82f49f14d7a4216146cd7f98df31727bc
colearrington98@gmail.com <colearrington98@gmail.com>
2023-08-13 15:49:02 -0500

    Created a basic css layout and. Troubleshooted and fixed the api.js and got the basic functionality working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './components/MovieList';
+import TVShowList from './components/TVShowList';
+import { fetchMovies, fetchTVShows } from './api';
+import './styles.css';
 
-function App() {
- 
-}
+const App = () => {
+  const [movies, setMovies] = React.useState([]);
+  const [tvshows, setTVShows] = React.useState([]);
+
+  const onSearchSubmit = async term => {
+    const movieResults = await fetchMovies(term);
+    setMovies(movieResults); 
+    const tvShowResults = await fetchTVShows(term);
+    setTVShows(tvShowResults); 
+  };
+  
+
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar onFormSubmit={onSearchSubmit} />
+      <MovieList movies={movies} />
+      <TVShowList tvshows={tvshows} />
+    </div>
+  );
+};
+
+
 
 export default App;
--- client/src/api.js
+++ client/src/api.js
@@ -1,15 +1,26 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = 'https://api.themoviedb.org/3';
 
+export const fetchMovies = async term => {
+    const response = await fetch(
+        `${API_URL}/search/movie?api_key=${API_KEY}&query=${term}`
+    );
+    const data = await response.json();
+    return data.results;
+};
+
+export const fetchTVShows = async term => {
+    const response = await fetch(
+        `${API_URL}/search/tv?api_key=${API_KEY}&query=${term}`
+    );
+    const data = await response.json();
+    return data.results;
+};
+
 const requests = {
-    fetchTrending: `${API_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `${API_URL}/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `${API_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=99`,
-}
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+};
 
 export default requests;
